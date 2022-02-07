2be33d6fe944e4eb6611de5e108462aa4fede7ba
Swen Mulderij <swenmulderij@gmail.com>
2022-02-07 20:29:00 +0100

    Refactored type selection

--- src/components/Hex.jsx
+++ src/components/Hex.jsx
@@ -1,17 +1,13 @@
 import { useState, useEffect } from "react";
-import { determineRender } from "../utils";
+import { determineRender, determineType } from "../utils";
 import { useDispatch } from "react-redux";
 import { select } from "../features/hexmap/hexmapSlice";
 
 const Hex = ({ hex_i, hex, hexElevation, hexTemp, hexVegetation, hexD }) => {
-  const [elevation, setElevation] = useState(null);
   const [hexPathStr, setHexPathStr] = useState(null);
   const [typePathStr, setTypePathStr] = useState(null);
-  const [color, setColor] = useState(null);
-  const [opacity, setOpacity] = useState(null);
   const [type, setType] = useState(null);
-  const [temp, setTemp] = useState(null);
-  const [vegetation, setVegetation] = useState(null);
+  const [render, setRender] = useState(null);
   const dispatch = useDispatch();
 
   const hexPath = (corners) => {
@@ -24,47 +20,51 @@ const Hex = ({ hex_i, hex, hexElevation, hexTemp, hexVegetation, hexD }) => {
   };
 
   useEffect(() => {
-    setElevation(hexElevation);
-    setTemp(hexTemp);
-    setVegetation(hexVegetation);
-    setHexPathStr(hexPath(hex.corners().map((cor) => cor.add(hex.toPoint()))));
-  }, [hex, hexD, hexElevation, hexTemp, hexVegetation]);
+    const type_choice = determineType(hexElevation, hexTemp, hexVegetation);
+    setType(type_choice);
+    const render = determineRender(type_choice);
+    setRender(render);
+    console.log(render);
+    setHexPathStr(hexPath(hex.corners()));
+    setTypePathStr(render.pathFn());
+  }, []);
 
-  useEffect(() => {
-    const { color, path, typename, opacity } = determineRender(
-      elevation,
-      temp,
-      vegetation
-    );
-    setColor(color);
-    setType(typename);
-    setTypePathStr(path);
-    setOpacity(opacity);
-  }, [elevation, temp, vegetation]);
-
-  return !(type == null) && !(color == null) ? (
+  return render ? (
     <g>
-      <path d={hexPathStr} fill={color} />
-      {typePathStr && (
-        <path
-          d={typePathStr}
-          stroke={"#000"}
-          strokeWidth={2}
-          vectorEffect="non-scaling-stroke"
-          opacity={opacity}
-          fill={"none"}
-          transform={`translate(${hex.toPoint().x},${hex.toPoint().y}) scale(${
-            hexD.w / 100
-          })`}
-        />
-      )}
+      <path
+        d={hexPathStr}
+        transform={`translate(${hex.toPoint().x},${hex.toPoint().y})`}
+        fill={render.color}
+      />
+
+      <path
+        d={typePathStr}
+        stroke={"#000"}
+        strokeWidth={2}
+        vectorEffect="non-scaling-stroke"
+        opacity={render.opacity}
+        fill={"none"}
+        transform={`translate(${hex.toPoint().x},${hex.toPoint().y}) scale(${
+          hexD.w / 100
+        })`}
+      />
+
       {/* RESIST THE TEMPTATION TO MAKE THIS ONHOVER !!!!! */}
       <path
         d={hexPathStr}
         fill="#fff"
         opacity={0}
-        onMouseUp={() => dispatch(select({ hex_i, type, temp, elevation }))}
-        onTouchEnd={() => dispatch(select({ hex_i, type, temp, elevation }))}
+        transform={`translate(${hex.toPoint().x},${hex.toPoint().y})`}
+        onMouseUp={() =>
+          dispatch(
+            select({ hex_i, type, hexTemp, hexElevation, hexVegetation })
+          )
+        }
+        onTouchEnd={() =>
+          dispatch(
+            select({ hex_i, type, hexTemp, hexElevation, hexVegetation })
+          )
+        }
       />
     </g>
   ) : (
