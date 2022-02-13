349a106e32b46a0b024909ce0cc86a3809e11c87
Swen Mulderij <swenmulderij@gmail.com>
2022-02-13 22:38:25 +0100

    Started on rivers

--- src/components/HexWorld.jsx
+++ src/components/HexWorld.jsx
@@ -7,9 +7,9 @@ import { determineHexValue } from "../features/storymaster/heuristic";
 import lifeforms from "../features/storymaster/lifeforms.json";
 // Turns the grid object into an array object, usefull for mapping
 const gridToArr = (grid) => {
-  const hexxes = [];
-  grid.forEach((hex) => hexxes.push(hex));
-  return hexxes;
+  const hexes = [];
+  grid.forEach((hex) => hexes.push(hex));
+  return hexes;
 };
 
 const HexWorld = ({ grid, rng }) => {
@@ -24,7 +24,7 @@ const HexWorld = ({ grid, rng }) => {
     const vegNoise = new SimplexNoise(rng());
     // offset to a random point in the noise
     const [x_off, y_off] = [rng(), rng()];
-    const terrain = grid.map((_hex) => {
+    let _world = grid.map((_hex) => {
       const [hexx, hexy] = [x_off + _hex.x, y_off + _hex.y];
       const el_val = elevationNoise.noise2D(hexx / 10, hexy / 10);
       const t_val = tempNoise.noise2D(hexx / 50, hexy / 50);
@@ -35,45 +35,90 @@ const HexWorld = ({ grid, rng }) => {
         temperature: t_val,
         vegetation: v_val,
         typeName: determineType(el_val, t_val, v_val),
+        river: null,
       });
     });
+    // -- GENERATE RIVERS
+    const river_count = Math.ceil(rng() * 3);
+    const highest_hexes = [...gridToArr(_world)]
+      .filter((_hex) => _hex.typeName.startsWith("mountain normal"))
+      .sort((fst, snd) => snd.elevation - fst.elevation)
+      .slice(0, river_count);
+    // const highest_el = Math.max(
+    //   ...gridToArr(_world)
+    //     .filter((_hex) => _hex.typeName.startsWith("mountain normal"))
+    //     .map((_hex) => _hex.elevation)
+    // );
+    // const highest_hexes = _world.filter(
+    //   (_hex) => _hex.elevation === highest_el
+    // );
+    highest_hexes.forEach((_hex) => _hex.set({ ..._hex, typeName: "highest" }));
+    // 945649
+    for (const high_hex of highest_hexes) {
+      let prev = null;
+      let cur = high_hex;
 
-    // -- GENERATE INHABITANTS--
-    const world = terrain.map((_hex) => {
-      const nbs = terrain.neighborsOf(_hex).filter(Boolean);
-
-      const kin_vals = {};
-      for (const kin of lifeforms) {
-        kin_vals[kin["kin-name-plural"]] = determineHexValue(
-          kin["type-preferences"],
-          _hex.typeName,
-          [_hex, ...nbs].map((h) => h.typeName)
+      while (true) {
+        const nbs = _world.neighborsOf(cur);
+        const edges = nbs.filter((_hex) => _hex === undefined);
+        if (edges.length > 0) {
+          cur.set({ ...cur, river: { from: prev, to: null } });
+          break;
+        }
+        const lowest_nbs_elevation = Math.min(
+          ...nbs.map((_hex) => _hex.elevation)
         );
-      }
-      return _hex.set({
-        ..._hex,
-        neighbors: nbs,
-        kin_vals: kin_vals,
-      });
-    });
+        const candidates = nbs.filter(
+          (_hex) => _hex.elevation === lowest_nbs_elevation
+        );
 
-    const worldArray = gridToArr(world);
-    for (const kin of lifeforms) {
-      const highest = Math.max(
-        ...worldArray.map((_hex) => _hex.kin_vals[kin["kin-name-plural"]])
-      );
-      const winners = world.filter(
-        (_hex) => _hex.kin_vals[kin["kin-name-plural"]] === highest
-      );
-      const winner = winners[Math.floor(rng() * winners.length)];
-      winner.set({ ...winner, typeName: kin.citytypes[0]["citytype-name"] });
+        const winner = candidates[Math.floor(rng() * candidates.length)];
+        cur.set({ ...cur, river: { from: prev, to: winner } });
+        if (winner.typeName.startsWith("water") || winner.river) {
+          break;
+        } else {
+          winner.set({ ...winner, typeName: "river" });
+          prev = cur;
+          cur = winner;
+        }
+      }
     }
+    // -- GENERATE INHABITANTS--
+    // const world = terrain.map((_hex) => {
+    //   const nbs = terrain.neighborsOf(_hex).filter(Boolean);
+
+    //   const kin_vals = {};
+    //   for (const kin of lifeforms) {
+    //     kin_vals[kin["kin-name-plural"]] = determineHexValue(
+    //       kin["type-preferences"],
+    //       _hex.typeName,
+    //       [_hex, ...nbs].map((h) => h.typeName)
+    //     );
+    //   }
+    //   return _hex.set({
+    //     ..._hex,
+    //     neighbors: nbs,
+    //     kin_vals: kin_vals,
+    //   });
+    // });
+
+    // const worldArray = gridToArr(world);
+    // for (const kin of lifeforms) {
+    //   const highest = Math.max(
+    //     ...worldArray.map((_hex) => _hex.kin_vals[kin["kin-name-plural"]])
+    //   );
+    //   const winners = world.filter(
+    //     (_hex) => _hex.kin_vals[kin["kin-name-plural"]] === highest
+    //   );
+    //   const winner = winners[Math.floor(rng() * winners.length)];
+    //   winner.set({ ...winner, typeName: kin.citytypes[0]["citytype-name"] });
+    // }
 
     console.timeEnd("worldgen");
     // BENCHMARK TRACKING
     // With Nbs: ~400ms
     // With Kin-prefs: ~420ms
-    setWorld(world);
+    setWorld(_world);
   }, [grid, rng]);
 
   return (
